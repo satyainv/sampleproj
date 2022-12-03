a6afc8d6c76f5afd0f2f2b10e5ba9badc3760a64
satyainv <44156624+satyainv@users.noreply.github.com>
2022-12-03 22:20:42 +0000

    my 7th commit

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test{
 
 public static void main(String args[]){
-System.out.println("hello yashas");
+System.out.println("hallo guten abend yashas!");
   System.out.println("Whats up?");
 
 }
