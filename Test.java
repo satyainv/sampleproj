daac7dcdaeecf55daa39c59ed112c24bd65194be
satyainv <44156624+satyainv@users.noreply.github.com>
2022-12-03 20:36:44 +0000

    third commit by yashu thalli

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test{
 
 public static void main(String args[]){
 System.out.println("hello yashas");
-  System.out.println("how are you");
+  System.out.println("are you fine?");
 
 }
 
