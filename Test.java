10a2bcb9c25bc57a4c376419ef1c42b07d78d7fb
satyainv <44156624+satyainv@users.noreply.github.com>
2022-12-04 18:48:05 +0000

    py commit

--- Test.java
+++ Test.java
@@ -4,6 +4,21 @@ public static void main(String args[]){
 System.out.println("hallo guten abend yashas!");
   System.out.println("Whats up?");
 
+
+  Secret py ingridient
+  Name= input("Hello what is your name? ")
+#print("ok " + Name)
+
+
+
+Colour= input("What is your favourite color? ")
+#print("Amazing colour! " + Colour)
+
+
+print("Your name is " + Name + "," + " and your favourite colour is " + Colour)
+
+
+
 }
 
 }
